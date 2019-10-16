d07db17f9fa185b1ca193e408dbf992cc321def6
Vikram <vikram2327@gmail.com>
2019-10-16 14:32:26 +0530

    first commit

---
+++ src/main/resources/static/precache-manifest.ceac4e88d83b22180afad51fe152fbc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb58ecd4c9e1634e4fd406cd1a4a2a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26a9476ffc09807d0c71",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "61be7a1aed221256e26e",
+    "url": "/static/js/2.a0cd5afd.chunk.js"
+  },
+  {
+    "revision": "26a9476ffc09807d0c71",
+    "url": "/static/js/main.dbb99164.chunk.js"
+  },
+  {
+    "revision": "54df56e84172a8377613",
+    "url": "/static/js/runtime-main.3dc57eb8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
